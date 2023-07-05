e029367e5d56b0ec40940fc48bd1565e5e9ab190
aba <bobsamy55@gmail.com>
2023-07-05 22:47:30 +0300

    commit project

---
+++ app/src/main/java/com/bob/cvdocapp/ui/activity/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.bob.cvdocapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bob.cvdocapp.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/bob/cvdocapp/ui/activity/HomeActivity.java
+++ app/src/main/java/com/bob/cvdocapp/ui/activity/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.bob.cvdocapp;
+package com.bob.cvdocapp.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.bob.cvdocapp.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
